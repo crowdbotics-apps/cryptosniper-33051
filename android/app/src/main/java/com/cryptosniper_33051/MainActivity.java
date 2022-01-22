72a63d9bd5ae6ad47411cd2f51ef72d3e6c2b8d3
Crowdbotics <team@crowdbotics.com>
2022-01-22 14:12:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cryptosniper_33051/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cryptosniper_33051;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cryptosniper_33051";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cryptosniper_33051",
+  url: "https://cryptosniper_33051.botics.co",
+  api: "https://cryptosniper_33051.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
